cdfa7470c3462c6c97c2b590604c74c4a5d8e210
AyeshaAI <119306537+AyeshaAI@users.noreply.github.com>
2023-07-13 21:29:57 +0100

    committing 7kyu solution

---
+++ Alan Partridge I - Partridge Watch - 7kyu/solution.py
@@ -0,0 +1,41 @@
+"""
+To celebrate today's launch of my Hero's new book: Alan Partridge: Nomad, We have a new series of kata arranged around the great man himself.
+
+Task
+Given an array of terms, if any of those terms relate to Alan Partridge, return Mine's a Pint!
+
+The number of exclamation mark (!) after the t should be determined by the number of Alan related terms you find in the given array (x). The related terms are as follows:
+
+Partridge
+PearTree
+Chat
+Dan
+Toblerone
+Lynn
+AlphaPapa
+Nomad
+If you don't find any related terms, return Lynn, I've pierced my foot on a spike!!
+
+"""
+
+def part(array):
+    alan_array = [
+        "Partridge",
+        "PearTree",
+        "Chat",
+        "Dan",
+        "Toblerone",
+        "Lynn",
+        "AlphaPapa",
+        "Nomad",
+    ]
+
+    exclaims = 0
+
+    for x in array:
+        if x in alan_array:
+            exclaims += 1
+    if exclaims >= 1:
+        return "Mine's a Pint" + "!" * exclaims
+    else:
+        return "Lynn, I've pierced my foot on a spike!!"
